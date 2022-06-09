c6a171de5ca6a7287dc3ed730e5a815725c1a296
RaviGopinath <gopinathravi95@gmail.com>
2022-06-09 17:09:58 +0530

    upload

---
+++ sumOfNumber1ton.js
@@ -0,0 +1,14 @@
+var x = prompt("Enter the Minimum Number :");
+var x = prompt("Enter the Maximum Number :")
+
+let i;
+let n = 17;
+
+
+for (i = 1; i <= n; i++) {
+    if (i % 3 == 0 || i % 5 == 0){
+    console.log(i);
+
+    }
+}
+
